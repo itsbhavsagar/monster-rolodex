599678fb19477ca612adc834ada4f8184a5db2ee
Bhavsagar <bhavsagar@protonmail.com>
2023-01-02 22:27:16 +0530

    Added "state & Shallow Merge"

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Yihua',
+      name: { firstName: 'Yihua', lastName: 'Zhang' },
+      company: 'ZTM',
     };
   }
 
@@ -17,10 +18,18 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button onClick={() => {
-           this.setState({name: 'Andrei'})
-          }}>Change Name</button>
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{' '}
+            {this.state.company}
+          </p>
+          <button
+            onClick={() => {
+              this.setState({ name: {firstName: 'Andrei', lastName: 'Neaogie'} });
+              console.log(this.state);
+            }}
+          >
+            Change Name
+          </button>
         </header>
       </div>
     );
